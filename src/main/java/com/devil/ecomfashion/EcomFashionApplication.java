b4460677a2731764269fd51170e94e8d88864b5b
techhimanshuupreti <upretihimanshu16@gmail.com>
2023-07-29 16:08:17 +0530

    initial commit

---
+++ src/main/java/com/devil/ecomfashion/EcomFashionApplication.java
@@ -0,0 +1,13 @@
+package com.devil.ecomfashion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomFashionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomFashionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devil/ecomfashion/EcomFashionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devil.ecomfashion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomFashionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
