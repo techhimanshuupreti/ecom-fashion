1dae8385a521e3d93f4d610b3740fdcf95742a98
techhimanshuupreti <upretihimanshu16@gmail.com>
2023-07-30 23:59:09 +0530

    initial commit

--- src/main/java/com/devil/ecomfashion/modules/auth/controller/AuthController.java
+++ src/main/java/com/devil/ecomfashion/modules/auth/controller/AuthController.java
@@ -1,14 +1,19 @@
 package com.devil.ecomfashion.modules.auth.controller;
 
+import com.devil.ecomfashion.modules.auth.dto.AuthDTO;
 import com.devil.ecomfashion.modules.auth.model.AuthResponse;
 import com.devil.ecomfashion.modules.auth.service.AuthService;
 import com.devil.ecomfashion.modules.user.UserDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -24,6 +29,16 @@ public class AuthController {
         return ResponseEntity.ok(authService.register(userDTO));
     }
 
-    
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> authenticate(@RequestBody AuthDTO authDTO){
+
+        return ResponseEntity.ok(authService.authenticate(authDTO));
+    }
+
+    @PostMapping("/refresh-token")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        authService.refreshToken(request,response);
+    }
 
 }
