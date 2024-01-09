3aadc188982ce3bcb6aca0d67834196e46e06b75
techhimanshuupreti <upretihimanshu16@gmail.com>
2024-01-09 16:27:24 +0530

    minor changes

--- src/main/java/com/devil/ecomfashion/exception/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/devil/ecomfashion/exception/CustomAuthenticationEntryPoint.java
@@ -1,25 +1,30 @@
 package com.devil.ecomfashion.exception;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
-    public void commence(HttpServletRequest req, HttpServletResponse res, AuthenticationException authException) throws IOException, ServletException {
+    public void commence(HttpServletRequest req, HttpServletResponse res, AuthenticationException authException) throws IOException {
         res.setContentType("application/json;charset=UTF-8");
         res.setStatus(403);
-//        JSONOBjec
-//        res.getWriter().write(new JsonObj //my util class for creating json strings
-//                .put("timestamp", new Timestamp(System.currentTimeMillis()))
-//                .put("status", 403)
-//                .put("message", "Access denied")
-//                .build());
+
+        //my util class for creating json strings
+        try {
+            JSONObject resJsonData = new JSONObject();
+            resJsonData.put("timestamp", Long.valueOf(System.currentTimeMillis()));
+            resJsonData.put("status", 403);
+            resJsonData.put("message", "Access denied");
+            res.getWriter().write(resJsonData.toString());
+        } catch (JSONException e) {
+            res.getWriter().write("message:Access denied");
+        }
     }
 }
