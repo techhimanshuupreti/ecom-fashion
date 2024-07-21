8a9cf209f5f0f92056bae69eab35ce5020cb58b6
techhimanshuupreti <upretihimanshu16@gmail.com>
2024-07-21 14:51:15 +0530

    add pom dependency

--- src/main/java/com/devil/ecomfashion/exception/CustomAccessDeniedHandler.java
+++ src/main/java/com/devil/ecomfashion/exception/CustomAccessDeniedHandler.java
@@ -3,12 +3,12 @@ package com.devil.ecomfashion.exception;
 import com.devil.ecomfashion.model.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.apache.commons.lang3.ObjectUtils;
-import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.util.MimeTypeUtils;
+import org.springframework.util.ObjectUtils;
 
 import java.io.IOException;
 
@@ -27,7 +27,7 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
             resJsonData.put("value", accessDeniedException.getMessage());
             res.getWriter().write(resJsonData.toString());
         } catch (JSONException e) {
-            res.getWriter().write(ObjectUtils.toString(new ApiResponse<String>().setMessage(accessDeniedException.getMessage())));
+            res.getWriter().write(ObjectUtils.getDisplayString(new ApiResponse<String>().setMessage(accessDeniedException.getMessage())));
         }
     }
 }
