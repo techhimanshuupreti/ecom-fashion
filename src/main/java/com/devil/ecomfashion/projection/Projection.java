d7be3adba6c2962e5ee75aeb53d1fdee99fa0ebb
upret <upretihimanshu16@gmail.com>
2024-08-10 18:48:45 +0530

    refactor the code

--- src/main/java/com/devil/ecomfashion/projection/Projection.java
+++
@@ -1,9 +0,0 @@
-package com.devil.ecomfashion.projection;
-
-import com.fasterxml.jackson.annotation.JsonInclude;
-
-@JsonInclude(JsonInclude.Include.NON_NULL)
-public interface Projection {
-    String getName();
-    Object getId();
-}
