dea0cc268b3257838ef3e550dd9a7f43a7ef8afe
upret <upretihimanshu16@gmail.com>
2024-08-10 20:16:55 +0530

    add endpoint for fetching products by category and sub-category

--- src/main/java/com/devil/ecomfashion/modules/subcategory/dto/request/SubCategoryDTO.java
+++ src/main/java/com/devil/ecomfashion/modules/subcategory/dto/request/SubCategoryDTO.java
@@ -1,8 +1,6 @@
 package com.devil.ecomfashion.modules.subcategory.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
@@ -10,13 +8,9 @@ import lombok.Data;
 @Builder
 public class SubCategoryDTO {
 
-    @NotNull(message = "Sub-Category name is not null")
-    @NotEmpty(message = "Sub-Category name is not empty")
     @NotBlank(message = "Sub-Category name is not blank")
     private String name;
 
-    @NotNull(message = "Category name is not null")
-    @NotEmpty(message = "Category name is not empty")
     @NotBlank(message = "Category name is not blank")
     private String categoryName;
 
